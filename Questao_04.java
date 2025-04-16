581d1dfb28f4acba7ade5737cf9550b9702cb85a
Jhonny <jhonnythomazz@gmail.com>
2025-04-16 14:13:09 -0300

    Questao_04.java

--- Questao_04.java
+++ Questao_04.java
@@ -8,11 +8,11 @@ public class Questao_04 {
 		Scanner ler = new Scanner(System.in);// importa a biblioteca de captura de dados
 		int num, result;// cria as variaveis
 
-		System.out.println("informe um valor: ");
-		num = ler.nextInt();
-		result = 0;
-		for(int i = 1; i <=10; i++) {
-			result = result + num;
-			System.out.println(result);
-		}ler.close();	}
+		System.out.println("informe um valor: ");// pede ao user um valor qualquer
+		num = ler.nextInt();// captura e atribui o valor a variavel "num"
+		result = 0; //define o valor da variavel
+		for(int i = 1; i <=10; i++) {// deixa em loop as informações entre as chaves
+			result = result + num;// define a variavel "result" somando a mesma com a variavel "num"
+			System.out.println(result);// projeta o resultado até chegar na quantidade de repetições pedidas(10)
+		}ler.close();// fecha a biblioteca de captura de dados	}
 }
